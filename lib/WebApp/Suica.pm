8434e274ef1cb8f37f8ad8f96719559bd73c77a4
cielo-ee <cielo.ee+github@gmail.com>
2015-11-22 23:33:05 +0900

    first commit

---
+++ lib/WebApp/Suica.pm
@@ -0,0 +1,114 @@
+package WebApp::Suica;
+
+use 5.006;
+use strict;
+use warnings;
+
+use File::Basename;
+
+use Encode;
+use utf8;
+use DBI;
+use Text::CSV_XS;
+
+our $VERSION='0.00001';
+
+sub new{
+    my ($class,%params) = @_;
+    my $filename = basename($params{'filename'});
+    $filename =~ /^(.+?)_/;
+    my $identifier = $1;
+    print $identifier;
+    my $dbname = "$identifier.db";
+    my $dbh    = DBI->connect("dbi:SQLite:dbname=$dbname");
+    
+    bless +{
+        dbh      => $dbh,
+        filepath => $params{'filename'},
+        fields   => undef
+    },$class;
+}
+
+
+sub init_db{
+    my $self = shift;
+    my $dbh  = $self->{'dbh'};
+    
+    my @main_fields = qw/id date agency s_station s_comp s_line e_station e_comp e_line fare balance /; #CSVに含まれているフィールド
+    my @option_fields = qw/filename registration_date /;
+    my @fields = (@main_fields,@option_fields);
+    my $fieldlist = join ',',@fields;
+    $self->{'fields'} = \@fields;
+    
+    $dbh->do("create table if not exists suica ($fieldlist,primary key(id,date))");
+    
+}
+
+
+sub register_csv2db{
+    my ($self,%params) = @_;
+    my $csvname = $self->{'filepath'};
+    my $dbh = $self->{'dbh'};
+    $self->init_db;
+
+    my @fields = @{$self->{'fields'}};
+
+
+    my ($mday,$mon,$year) = (localtime(time))[3..5];
+    $year += 1900;
+    $mon++;
+    my $date = sprintf "%d/%02d/%02d",$year,$mon,$mday;
+    
+    open my $fh,'<',$csvname or die "$!";
+    my $csv = Text::CSV_XS-> new({binary => 1});
+
+    while(my $columns = $csv->getline($fh)){
+        #ハッシュにマッピング
+        my $eles;
+        @$eles{@fields} = @$columns;
+        $eles->{'filename'} = $csvname;
+        $eles->{'registration_date'}     = $date;
+
+        #タイトル行を飛ばす
+        next if($eles->{'id'} eq "ID");
+
+        my $valuelist = $eles->{'id'};
+        foreach my $i(1..$#fields){
+            my $value = decode('UTF-8',$eles->{$fields[$i]});
+            $value =~ s/￥|,//g if($fields[$i] eq 'fare');
+            $value =~ s/￥|,//g if($fields[$i] eq 'balance');
+            $valuelist .= ",\'".$value."\'";
+        }
+
+        #重複データを検査する
+        my $stmt = "select * from suica where id = $columns->[0]";
+        my $sth  = $dbh -> prepare($stmt);
+        $sth -> execute;
+        next if($sth->fetchrow_array);
+
+        my $fieldlist = join ',',@fields;
+        $dbh->do("insert into suica ($fieldlist) values ($valuelist)");
+    }
+
+    $csv->eof;
+}
+
+
+
+sub show_all_db{
+    my $self = shift;
+    my $dbh  = $self->{'dbh'};
+    my $stmt = "select * from suica order by id asc";
+    
+    my $sth  = $dbh->prepare($stmt);
+    $sth -> execute;
+    
+    while(my @row = $sth->fetchrow_array){
+        print join '|',@row;
+        print "\n";
+    }
+}
+__END__
+
+
+
---
+++ script/debug.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+use FindBin qw($Bin);
+use lib "$Bin/../lib/";
+
+use WebApp::Suica;
+use Data::Dumper;
+
+my $filename = shift;
+
+my $db = WebApp::Suica->new(filename => $filename);
+
+$db->register_csv2db;
+
+$db->show_all_db;
+
+$db->{'dbh'}->disconnect;
+
